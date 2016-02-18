ffb0d5a8d4aa800bbdf1b5710a31a33e2410cb39
Chris Mark <chdmark@gmail.com>
2016-02-17 17:47:54 -0800

    Make object an array

--- application.js
+++ application.js
@@ -27,14 +27,48 @@ var commands =
 
 var makeSchedule = function(time, events) {
 	var parseTime = time;
-	var start = parseTime.startTime;
-	var end = parseTime.endTime;
-	console.log(start);
-	console.log(end);
+	var start = convertTimestamp(parseTime.startTime);
+	var end = convertTimestamp(parseTime.endTime);
+	
 	
 	$(".start").children(".left").append(start);
+	$(".start").children(".middle").append($("<i class='fa fa-circle'></i>"));
 	$(".start").children(".right").append("Class Session Started");
 	$(".end").children(".left").append(end);
+	$(".end").children(".middle").append($("<i class='fa fa-circle'></i>"));
 	$(".end").children(".right").append("Class Session Ended");
-}
 
+	var newArray = $.each(events, function(n){
+		$.makeArray(n)
+	});
+	console.log(newArray);
+	debugger
+};
+
+
+var convertTimestamp = function(timestamp) {
+  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+		yyyy = d.getFullYear(),
+		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+		hh = d.getHours(),
+		h = hh,
+		min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+		ampm = 'AM',
+		time;
+			
+	if (hh > 12) {
+		h = hh - 12;
+		ampm = 'PM';
+	} else if (hh === 12) {
+		h = 12;
+		ampm = 'PM';
+	} else if (hh == 0) {
+		h = 12;
+	}
+	
+	// ie: 2013-02-18, 8:35 AM	
+	time = h + ':' + min + ' ' + ampm;
+		
+	return time;
+};
